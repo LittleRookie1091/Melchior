71fcaf709eac96090bc0b4928282a90c740a6ce9
LittleRookie1091 <akatsuki1091@gmail.com>
2021-01-08 11:30:21 +1300

    Added javasrcipt

---
+++ Melchior/wwwRoot/js/index.js
@@ -0,0 +1,34 @@
+﻿const uri = 'api/melchior';
+let melch = [];
+
+function getProfiles() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
+
+function addProfile() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const profile = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(profile)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getProfiles();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
