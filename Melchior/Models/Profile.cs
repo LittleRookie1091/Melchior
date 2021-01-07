1554f5d8344f65abc8646e2b6da9987460dc3a6a
LittleRookie1091 <akatsuki1091@gmail.com>
2021-01-07 22:14:23 +1300

    Creating profile class in Melchior

--- Melchior/Models/Profile.cs
+++ Melchior/Models/Profile.cs
@@ -7,5 +7,11 @@ namespace Melchior
 {
     public class Profile
     {
+        public string Name { get; set; }
+        public int Age{ get; set; }
+        public string Description { get; set; } 
+        public bool IsComplete { get; set; }
+
+        public string Secret { get; set; }
     }
 }
